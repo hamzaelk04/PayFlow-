8463048b80a7885c28299eca9b0a9110c83f0528
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-03-09 11:12:52 +0000

    add the fillable and the relations in the book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'author', 'isbn', 'published_year', 'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
